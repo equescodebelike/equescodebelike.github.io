134229f35e7301d550cd4229225e19b3fb5a83d0
Arkhip Korobov <equescodebelike@Mac.lan>
2025-11-30 22:18:35 +0300

    alpha version

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: { unoptimized: true }
+  output: "export",
+  images: { unoptimized: true },
+  // Для GitHub Pages (проектный репозиторий "core")
+  basePath: isProd ? "/core" : undefined,
+  assetPrefix: isProd ? "/core/" : undefined,
 };
 
 export default nextConfig;
